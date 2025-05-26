91437ec583d3dd22e31bdebdbef00a850cd90266
fwj <fwj-185@qq.com>
2025-05-26 08:43:47 +0800

    修改一些参数

--- Assets/Scripts/Game/Enemy/Enemy.cs
+++ Assets/Scripts/Game/Enemy/Enemy.cs
@@ -54,6 +54,18 @@ namespace PrjectSurvivor
                 var dir = (Player.Default.transform.position - transform.position).normalized;
                 //var dirNor = (Player.Default.transform.position - transform.position).magnitude;
                 //if()
+
+                // 计算朝向玩家的角度
+                // Mathf.Atan2 返回的是弧度，需要转换为角度
+                // dir.y 是Y轴方向，dir.x 是X轴方向
+                float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+
+                // 应用旋转，使敌人朝向玩家
+                // 对于2D游戏，通常是绕Z轴旋转
+                // 如果您的敌人Sprite默认是朝右的，这个角度是正确的
+                // 如果默认朝向不同，您可能需要调整角度，例如默认朝上则需要 angle - 90f
+                transform.rotation = Quaternion.Euler(0f, 0f, angle);
+
                 SelfRigidbody2D.velocity = dir * MoveSpeed;
             }
             else
